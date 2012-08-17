a41d48092e70302a7e967f31abafcc6bd9be56a7
Amauri Rodrigues <amauri.rodrigues@gmail.com>
2012-08-17 18:54:46 -0300

    Update OlaMundo.java

--- OlaMundo.java
+++ OlaMundo.java
@@ -2,6 +2,7 @@ public class OlaMundo{
 	public static void main(String Args[]){
 		OlaMundo olaMundo = new OlaMundo();
 		olaMundo.falar("Ola Mundo, eu tenho metodo....");
+                  olaMundo.falar("Esta linha nao existia....");
 	}
 	
 	public void falar(String frase){
